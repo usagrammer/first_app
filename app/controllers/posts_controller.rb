959fe3800c65c0ed5c55dcb410a700d4d2a63cc6
usagrammer <uny0102@gmail.com>
2020-07-02 11:23:06 +0900

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order(id: "DESC")
+  end
+
+  def new
+  end
+
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
+
+  def checked
+
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json:{ post: item }
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'posts#index'
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  get 'posts/?id=id', to: 'posts#checked'
+  get 'posts/:id', to: 'posts#checked'
+end
